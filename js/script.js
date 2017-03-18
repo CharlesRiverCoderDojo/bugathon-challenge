1b759bd2635209322e71145e6c066cd5cbede232
andreasantarl <andrea.santarl@gmail.com>
2017-03-18 15:16:13 -0400

    Adds two functions that reverse name
    User enters name
    Two functions reverse the name
    Reversed name displays in empty divs

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
-// function checkNumber () {
-//   let age = document.getElementById('age-input').value;
-//   console.log(age);
-// }
+// first function that reverses name that user puts into form
+function reverseName1(name) {
+  // let nameForward = name;
+  let name1 = name.split("").reverse().join("");
+  $("#message1").html("Your name reversed is " + name1 + ".");
+}
 
+// second function that reverses name that user puts into form
+function reverseName2(name) {
+    var name2 = "";
+    for (var i = name.length - 1; i >= 0; i--) {
+        name2 += name[i];
+    }
+    $("#message2").html("Your name backwards is still " + name2);
+}
 
-// $("button").on('click', function(){
-//   let age = document.getElementById('age-input').value;
-//   if (isNaN(age)){
-//     $("#age-in-days").html("Please enter a number");
-//   } else {
-//     let age-to-days = age
-//   }
-// })
-
-// reverses name that user puts into form
+// display reversed names
 $("button").on('click', function(){
   let name = document.getElementById('name-input').value;
-  let nameReversed = name.split("").reverse().join("");
-  $("#message").html("Your name reversed is " + nameReversed + ". Thank you for stopping by, " + name + ".");
-
+  reverseName1(name);
+  reverseName2(name);
+  $("#thank-you").html("<br>Thank you for stopping by, " + name + ".");
 });
